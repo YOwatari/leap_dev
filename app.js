e309673dba70dfa1e86b3e45f49f62a60313d576
YOwatari <yyutao88@gmail.com>
2013-11-03 22:41:24 +0900

    お絵かきチャット

--- app.js
+++ app.js
@@ -4,33 +4,49 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
 var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
 
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.configure(function () {
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', path.join(__dirname, 'views'));
+	app.set('view engine', 'ejs');
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.json());
+	app.use(express.urlencoded());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(path.join(__dirname, 'public')));
+});
 
-// development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+// socket.io
+io.sockets.on('connection', function (client) {
+	console.log('Connection')
+	client.on('message', function (data) {
+		console.log('Broadcast');
+		return client.broadcast.emit('message', data);
+	});
+	return client.on('disconnect', function () {
+		return console.log('Disconnect');
+	});
 });
+
+app.get('/', function (req, res) {
+	res.render('index', {
+		title: 'sketch!'
+	});
+});
+
+if (!module.parent) {
+	server.listen(app.get('port'));
+}
