5a394e3551d12817f86dc6994bc5d1ddb2515117
YOwatari <yyutao88@gmail.com>
2013-11-21 23:05:48 +0900

    だいぶ完成

--- public/javascripts/top.js
+++ public/javascripts/top.js
@@ -27,35 +27,37 @@ $(function () {
 	var fps = new FPS(30);
 
 	// canvas
-	$canvas = $("canvas")
-	$canvas.css({
-		border: "1px solid black",
-		background: "gray"
-	});
+	var Layers = [];
+	Layers.push($("canvas#layer0"));
+	Layers.push($("canvas#layer1"));
+	Layers.push($("canvas#layer2"));
+	Layers.push($("canvas#layer3"));
 
 	// retina対応
 	var scale = 1;
 	if (window.devicePixelRatio == 2) {
-		$canvas.attr({
-			width: 800,
-			height: 600
-		});
-		
-		$canvas.scaleCanvas({
-			scale: 2
+		$.each(Layers, function (index, value) {
+			value.attr({
+				width: 800,
+				height: 600
+			});
+
+			value.scaleCanvas({
+				scale: 2
+			});
 		});
 		
 		scale = 2;
 	}
 
 	// 画面領域
-	var canvasX = $canvas.width();
-	var canvasY = $canvas.height();
-	console.log("画面", canvasX, canvasY);
-
+	var canvasX = Layers[0].width();
+	var canvasY = Layers[0].height();
 	var centerX = canvasX / 2;
 	var centerY = canvasY / 2;
-	console.log("中心", centerX, centerY);
+
+	var NEXT = false;
+	var nextGame = false;
 
 	// 指
 	var Finger = function () {
@@ -64,8 +66,8 @@ $(function () {
 		this.r = 5;
 	};
 	Finger.prototype = {
-		draw: function () {
-			$canvas.drawArc({
+		draw: function (layer) {
+			layer.drawArc({
 				fillStyle: "green",
 				x: this.x/scale, y: this.y/scale,
 				radius: this.r
@@ -74,51 +76,58 @@ $(function () {
 	};
 
 	// Ring
-	var Ring = function (r, c) {
-		this.x = centerX;
-		this.y = centerY;
+	var Ring = function (x, y, r, c, target) {
+		this.x = x;
+		this.y = y;
 		this.r = r;
 		this.c = c;
+		this.target = target;
 	};
 	Ring.prototype = {
-		draw: function () {
-			$canvas.drawArc({
-				strokeStyle: r1.collision(f1) ? "red" : this.c,
+		draw: function (layer) {
+			layer.drawArc({
+				strokeStyle: this.collision() ? "red" : this.c,
 				strokeWidth: 4,
 				x: this.x/scale, y: this.y/scale,
 				radius: this.r
 			});
 		},
-		collision: function (target) {
-			var dx = target.x - this.x;
-			var dy = target.y - this.y;
+		collision: function () {
+			var dx = this.target.x - this.x;
+			var dy = this.target.y - this.y;
 			var distance = Math.sqrt(dx*dx + dy*dy);
-			var surface = this.r + target.r;
+			var surface = this.r + this.target.r;
 
 			return distance < surface;
 		}
 	};
 
-	var Arrow = function (direction) {
-		this.x = 0;
-		this.y = canvasY/4;
-		this.c = "blue";
+	var Arrow = function (x, y, color, direction) {
+		if (x <= 0) {
+			this.x = x - canvasX/4;
+		} else {
+			this.x = x + canvasX/4;
+		}
+		this.x = x;
+		this.y = y;
+		this.c = color;
 		if (direction == "right") {
 			this.r = 45;
-			this.m = 10
+			this.m = 10;
 		} else {
 			this.r = -45;
 			this.m = -10;
 		};
 	};
 	Arrow.prototype = {
-		draw: function () {
-			$canvas.drawRect({
+		draw: function (layer) {
+			layer.drawRect({
 				fillStyle: this.c,
 				x: (this.x - canvasX*0.25/4)/scale,
 				y: (this.y - canvasX*0.25/4)/scale,
 				width: canvasX/scale*0.25, height: canvasY/scale*0.1,
 				rotate: this.r,
+				cornerRadius: 10
 			})
 			.drawRect({
 				fillStyle: this.c,
@@ -126,6 +135,7 @@ $(function () {
 				y: (this.y + canvasX*0.25/4)/scale,
 				width: canvasX/scale*0.25, height: canvasY/scale*0.1,
 				rotate: -this.r,
+				cornerRadius: 10
 			});
 
 			this.update();
@@ -134,18 +144,125 @@ $(function () {
 			this.x = this.x + this.m;
 
 			if (this.m > 0) {
-				if (this.x > canvasX + canvasX*0.25)
-				this.x = - canvasX*0.25;
+				if (this.x > canvasX + canvasX/4)
+					this.x = -canvasX;
 			} else {
-				if (this.x < -canvasX*0.25)
-				this.x = canvasX + canvasX*0.25;
+				if (this.x < -canvasX/4)
+					this.x = 2*canvasX;
+			}
+		}
+	};
+
+	// Arc
+	var Arc = function (color) {
+		this.x = 0;
+		this.y = 0;
+		this.c = color;
+		this.direction = 0;
+		this.radius = 0;
+		this.speed = 1.5;
+
+		this.init();
+	};
+	Arc.prototype = {
+		init: function () {
+			this.x = Math.floor( Math.random() * canvasX );
+			this.y = Math.floor( Math.random() * canvasY );
+			this.direction = Math.floor( Math.random() * 2*Math.PI );
+			this.radius = 30 + Math.floor( Math.random() * 70 );
+			this.speed = 1 + Math.floor( Math.random() );
+		},
+		draw: function (layer) {
+			layer.drawArc({
+				fillStyle: this.c,
+				x: this.x/scale, y: this.y/scale,
+				radius: this.radius
+			});
+
+			this.update();
+		},
+		update: function () {
+			this.x = this.x + this.speed*Math.sin(this.direction);
+			this.y = this.y + this.speed*Math.cos(this.direction);
+
+			if (this.x - this.radius*2 > canvasX
+				|| this.x + this.radius*2 < 0
+				|| this.y - this.radius*2 > canvasY
+				|| this.y + this.radius*2 < 0) {
+				this.init();
+			}
+		}
+	};
+
+	var Curtain = function (layer) {
+		this.wL = 0;
+		this.wR = 0;
+		this.c = "white";
+		this.openSpeed = 15;
+		this.l = layer;
+	};
+	Curtain.prototype = {
+		draw: function (layer) {
+			layer.drawRect({
+				fillStyle: this.c,
+				x: centerX/scale,
+				y: 0,
+				width: this.wL/scale, height: canvasY/scale,
+				fromCenter: false
+			});
+
+			layer.drawRect({
+				fillStyle: this.c,
+				x: centerX/scale,
+				y: 0,
+				width: this.wR/scale, height: canvasY/scale,
+				fromCenter: false
+			});
+
+			this.update();
+		},
+		update: function () {
+			if (this.wL > -centerX || this.wR < centerX) {
+				this.wL -= this.openSpeed;
+				this.wR += this.openSpeed;
+			} else {
+				NEXT = false;
 			}
 		}
 	};
 
 	var f1 = new Finger();
-	var r1 = new Ring(8, "blue");
-	var a1 = new Arrow("right");
+	var r1 = new Ring(centerX-150, centerY+25, 10, "blue", f1);
+	var r2 = new Ring(centerX+150, centerY+25, 10, "green", f1);
+
+	var aR1 = new Arrow(0, canvasY/4, "blue", "right");
+	var aR2 = new Arrow(-canvasX*1/4, canvasY/4, "blue", "right");
+	var aR3 = new Arrow(-canvasX*2/4, canvasY/4, "blue", "right");
+	var aR4 = new Arrow(-canvasX*3/4, canvasY/4, "blue", "right");
+
+	var aL1 = new Arrow(canvasX, canvasY*3/4, "red", "left");
+	var aL2 = new Arrow(canvasX + canvasX*1/4, canvasY*3/4, "red", "left");
+	var aL3 = new Arrow(canvasX + canvasX*2/4, canvasY*3/4, "red", "left");
+	var aL4 = new Arrow(canvasX + canvasX*3/4, canvasY*3/4, "red", "left");
+
+	var colors = [
+		"#ffc6c6",
+		"#ffc6e2",
+		"#ffc6ff",
+		"#e2c6ff",
+		"#c6c6ff",
+		"#c6e2ff",
+		"#c6ffff",
+		"#c6ffe2",
+		"#c6ffc6",
+		"#e2ffc6",
+		"#ffffc6",
+		"#ffe2c6"
+	];
+	var circle = $.map(colors, function (value, index) {
+		return new Arc(value);
+	});
+	var curtain = new Curtain();
 
 	// Laep Motion
 	Leap.loop(function (frame) {
@@ -155,15 +272,91 @@ $(function () {
 		}
 	});
 
+	var Layer0Update = function L0Update () {
+		for (var i = circle.length - 1; i >= 0; i--) {
+			circle[i].draw(Layers[0]);
+		};
+
+		Layers[0].drawRect({
+			fillStyle: "rgba(255,255,255,0.7)",
+			x: centerX/scale,
+			y: centerY/scale,
+			width: 0.9*canvasX/scale, height: 0.9*canvasY/scale,
+			cornerRadius: 10
+		})
+	};
+
+	var Layer1Update = function L1Update () {
+		// sub title
+		Layers[1].drawText({
+			fillStyle: "black",
+			fontStyle: "bold",
+			fontSize: "8pt",
+			x: 0.75*centerX/scale, y: (centerY-150)/scale,
+			text: "瞬間バディパーティゲーム"
+		});
+
+		// title
+		Layers[1].drawText({
+			fillStyle: "black",
+			fontStyle: "bold",
+			fontSize: "30pt",
+			x: centerX/scale, y: (centerY-100)/scale,
+			text: "ユビフレ"
+		});
+
+		// start
+		Layers[1].drawText({
+			fillStyle: "black",
+			fontStyle: "bold",
+			fontSize: "15pt",
+			x: centerX/scale, y: (centerY+150)/scale,
+			text: "hold your finger and slide"
+		});
+	};
+
+	var Layer2Update = function L2Update () {
+		r1.draw(Layers[2]);
+		r2.draw(Layers[2]);
+
+		//aR1.draw();
+		//aR2.draw();
+		//aR3.draw();
+		//aR4.draw();
+
+		//aL1.draw();
+		//aL2.draw();
+		//aL3.draw();
+		//aL4.draw();
+	};
+
+	var Layer3Update = function L3Update () {
+		f1.draw(Layers[3]);
+	};
+	Layers[3].bind("click", function(){
+		NEXT = true;
+	});
+
+
 	// 画面更新
 	var update = function update () {
 		fps.check();
-		$canvas.clearCanvas();
 
-		a1.draw();
+		$.each(Layers, function (index, value) {
+			value.clearCanvas();
+		});
+
+		Layer0Update();
+		Layer1Update();
+		Layer2Update();
+		Layer3Update();
+
+		if (NEXT)
+			curtain.draw(Layers[2]);
+		
 
 		// fps
-		$canvas.drawText({
+		Layers[3].drawText({
 			fillStyle: "black",
 			fontStyle: "bold",
 			fontSize: "5pt",
@@ -172,31 +365,15 @@ $(function () {
 			text: "FPS: " + fps.getFPS()
 				+ "\nf: " +  ("00"+(f1.x)).slice(-3) + " " + ("00"+(f1.y)).slice(-3)
 		});
-		
-		// title
-		$canvas.drawText({
-			fillStyle: "black",
-			fontStyle: "bold",
-			fontSize: "30pt",
-			x: centerX/scale, y: (centerY-100)/scale,
-			text: "Hello World."
-		});
-
-		// start
-		$canvas.drawText({
-			fillStyle: "white",
-			fontStyle: "bold",
-			fontSize: "15pt",
-			x: centerX/scale, y: (centerY+100)/scale,
-			text: "hold your finger"
-		});
-
-		r1.draw();
-
-		f1.draw();
 
 		setTimeout(update, fps.getInterval());
 	};
 
-	update();
+	var nextUpdate = update();
+
+	if (!nextGame) {
+		update();
+	} else {
+		nextUpdate();
+	}
 });
