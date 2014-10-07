a0fd21985fc223b9a1d5df5c558151d333b6a6a8
Yuta OHWATARI <yyutao88@gmail.com>
2014-10-07 14:10:12 +0900

    Revert "お絵かきチャット"
    This reverts commit e309673dba70dfa1e86b3e45f49f62a60313d576.
    
    Conflicts:
    	app.js
    	package.json
    	views/index.ejs

--- app.js
+++ app.js
@@ -10,40 +10,27 @@ var http = require('http');
 var path = require('path');
 
 var app = express();
-var server = http.createServer(app);
-// var io = require('socket.io').listen(server);
 
 // all environments
-app.configure(function () {
-	app.set('port', process.env.PORT || 3000);
-	app.set('views', path.join(__dirname, 'views'));
-	app.set('view engine', 'ejs');
-	app.use(express.favicon());
-	app.use(express.logger('dev'));
-	app.use(express.json());
-	app.use(express.urlencoded());
-	app.use(express.methodOverride());
-	app.use(app.router);
-	app.use(express.static(path.join(__dirname, 'public')));
-});
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 
+// development only
 if ('development' == app.get('env')) {
-	app.use(express.errorHandler());
+  app.use(express.errorHandler());
 }
-
-/*// socket.io
-io.sockets.on('connection', function (client) {
-	console.log('Connection')
-	client.on('message', function (data) {
-		console.log('Broadcast');
-		return client.broadcast.emit('message', data);
-	});
-	return client.on('disconnect', function () {
-		return console.log('Disconnect');
-	});
-});*/
 
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-server.listen(app.get('port'), process.env.IP);
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
