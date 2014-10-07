3537ce26df37b89c7466778411e0489744d137d1
Yuta OHWATARI <yyutao88@gmail.com>
2014-10-07 15:14:30 +0900

    Fix: ポート設定を修正

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var io = require('socket.io').listen(server);
 
 // all environments
 app.configure(function () {
-	app.set('port', process.env.PORT);
+	app.set('port', process.env.PORT || '3000');
 	app.set('views', path.join(__dirname, 'views'));
 	app.set('view engine', 'ejs');
 	app.use(express.favicon());
